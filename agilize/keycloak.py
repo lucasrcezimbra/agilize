1f50a91558233deee139a9951f59b41f69385247
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-04-16 12:30:43 -0300

    Add Keycloak

---
+++ agilize/keycloak.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class Keycloak:
+    PATH_TOKEN = "realms/{realm_name}/protocol/openid-connect/token"
+
+    def __init__(self, base_url, client_id, realm_name):
+        self.base_url = base_url
+        self.client_id = client_id
+        self.realm_name = realm_name
+        self.session = requests.Session()
+
+    def token(self, username, password):
+        url = self.base_url + self.PATH_TOKEN.format(realm_name=self.realm_name)
+        payload = {
+            "username": username,
+            "password": password,
+            "client_id": self.client_id,
+            "grant_type": ['password'],
+        }
+        return self.session.post(url, data=payload).json()
+
+    def url(self, type):
+        return self.base_url + self.path(type)
+
+    def path(self, type):
+        return {
+            'token': self.PATH_TOKEN.format(realm_name=self.realm_name),
+        }[type]
---
+++ tests/test_keycloak.py
@@ -0,0 +1,66 @@
+from uuid import uuid4
+
+import pytest
+import requests
+import responses
+
+from agilize.keycloak import Keycloak
+
+
+@pytest.fixture
+def keycloak():
+    base_url = 'https://sso.agilize.com.br/auth/'
+    client_id = 'agilize-legacy-client'
+    realm_name = 'AgilizeAPPs'
+    return Keycloak(base_url, client_id, realm_name)
+
+
+def test_init():
+    keycloak = Keycloak('BASE_URL', 'CLIENT_ID', 'REALM_NAME')
+
+    assert keycloak.base_url == 'BASE_URL'
+    assert keycloak.client_id == 'CLIENT_ID'
+    assert keycloak.realm_name == 'REALM_NAME'
+    assert isinstance(keycloak.session, requests.Session)
+
+
+def test_path(keycloak):
+    assert keycloak.path('token') == Keycloak.PATH_TOKEN.format(realm_name=keycloak.realm_name)
+
+
+def test_url(keycloak):
+    assert keycloak.url('token') == keycloak.base_url + keycloak.path('token')
+
+
+def test_token_call_post(keycloak, mocker):
+    post_spy = mocker.patch('requests.Session.post')
+
+    username, password = 'cnpj', 'password'
+    keycloak.token(username, password)
+
+    post_spy.assert_called_once_with(
+        keycloak.url('token'),
+        data={
+            "username": username,
+            "password": password,
+            "client_id": keycloak.client_id,
+            "grant_type": ['password'],
+        }
+    )
+
+
+@responses.activate
+def test_token(keycloak):
+    response = {
+        'access_token': 'random_string',
+        'expires_in': 600,
+        'refresh_expires_in': 1800,
+        'refresh_token': 'other_random_string',
+        'token_type': 'Bearer',
+        'not-before-policy': 0,
+        'session_state': str(uuid4()),
+        'scope': 'profile roles email'
+    }
+    responses.add(responses.POST, keycloak.url('token'), json=response)
+
+    assert keycloak.token('username', 'password') == response
