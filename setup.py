263b8cd1b7893079b4cdb435009fe3546323b865
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-11-26 15:52:06 -0300

    Bump to 0.0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     setup(
         name='Agilize',
         description='Unofficial client to access Agilize',
-        version='0.0.7',
+        version='0.0.8',
         long_description=open(README).read(),
         long_description_content_type='text/markdown',
         author="Lucas Rangel Cezimbra",
